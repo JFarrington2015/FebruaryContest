a54336abb1c565ca7b60206499337be2fbef538c
jf4rringt0n <jfarring@maine.rr.com>
2020-02-13 23:28:35 -0500

    contest try 1

--- FebruaryContestEntry/Program.cs
+++ FebruaryContestEntry/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Dynamic;
+using System.Runtime.InteropServices;
 
 namespace FebruaryContestEntry
 {
@@ -10,11 +12,71 @@ namespace FebruaryContestEntry
 		{
 			// The solution implementation goes here
 
+			var boxBuilder = new BoxBuilder()
+                .SetDimensions(30, 15)
+                .SetBorderColor(ConsoleColor.Green)
+                .SetInsetColor(ConsoleColor.Yellow);
+
+            for (var y=1;y<=boxBuilder.Height;y++) {
+                for (var x = 1; x <= boxBuilder.Width; x++)
+                {
+                    Console.ForegroundColor = boxBuilder.SetCoords(x, y).GetBlockColor();
+                    Console.Write(block);
+                }
+                Console.WriteLine();
+            }
+
 			Console.ResetColor();
 			Console.WriteLine();
 			Console.WriteLine();
 			Console.WriteLine("Yay, a colorful shape!");
 			Console.ReadLine();
 		}
+
+        class BoxBuilder
+        {
+            private int X { get; set; }
+            private int Y { get; set; }
+            public int Width { get; set; }
+            public int Height { get; set; }
+            private ConsoleColor BorderColor { get; set; }
+            private ConsoleColor InsetColor { get; set; }
+
+            public BoxBuilder SetDimensions(int width, int height)
+            {
+                Width = width;
+                Height = height;
+                return this;
+            }
+
+            public BoxBuilder SetCoords(int x, int y)
+            {
+                X = x;
+                Y = y;
+                return this;
+            }
+
+            public bool IsBorder()
+            {
+                return (X == 1 || X == Width || Y ==1 || Y == Height);
+            }
+
+            public BoxBuilder SetBorderColor(ConsoleColor color)
+            {
+                BorderColor = color;
+                return this;
+            }
+
+            public BoxBuilder SetInsetColor(ConsoleColor color)
+            {
+                InsetColor = color;
+                return this;
+            }
+
+            public ConsoleColor GetBlockColor()
+            {
+                return this.IsBorder() ? BorderColor : InsetColor;
+            }
+        }
 	}
 }
